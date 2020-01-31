232b39114c8bed9e07f22bd6b80c4c9a93e123d1
Roginsky <Evgeny.Roginsky@kaspersky.com>
2020-01-31 18:01:03 +0300

    removed old legacy code since HTML scraping doesn't work anymore

--- boardcamp.py
+++ boardcamp.py
@@ -7,7 +7,6 @@ import json
 import errno
 import gevent
 import requests
-from bs4 import BeautifulSoup
 from collections import defaultdict
 
 import urllib3
@@ -33,77 +32,10 @@ def read_file_if_exists(filename):
 data = read_file_if_exists(game_name_cache_file)
 game_name_cache = json.loads(data) if data else {}
 
-def get_html(path):
-	data = read_file_if_exists('%s/%s' % (pagedir, path.replace('/', '\\')))
-	if not data:
-		print("Reading page", path)
-		res = requests.get('%s%s' % (site, path))
-		res.raise_for_status()
-		data = res.text
-		with open('%s/%s' % (pagedir, path.replace('/', '\\')), 'w') as f:
-			f.write(data)
-	return BeautifulSoup(data, 'html.parser')
-
-def extract_game_title(path):
-	name = game_name_cache.get(path)
-	if name:
-		print("Game %s found in cache" % name)
-		return name
-	print("Extracting game name from %s" % path)
-	html = get_html(path)
-	name = html.find('h1', id='game_title').span.getText().strip()
-	game_name_cache[path] = name
-	return name
-
-def find_next_page(html):
-	pagination = html.find('div', class_='pagination')
-	if not pagination: return
-	arrow = pagination.find('img')
-	if not arrow or arrow['src'] != '/img/br.gif': return
-	ref = arrow.parent
-	if ref.name != 'a':
-		print("Something went wrong, arrow's name isn't a")
-		return
-	return ref['href']
-
-def extract_games(html):
-	for div in html.find_all('div', class_='gameslinked'):
-		game_ref = div.find('div', class_='text').find('a')['href']
-		yield extract_game_title(game_ref)
-
-def get_games(html):
-	yield from extract_games(html)
-	while True:
-		next_page = find_next_page(html)
-		if next_page:
-			html = get_html(next_page)
-			yield from extract_games(html)
-		else:
-			break
-
-def get_user_games(user):
-	tmpfile = '%s/%s.tmp' % (tmpdir, user)
-	userfile = '%s/%s.games' % (tmpdir, user)
-	try:
-		with open(userfile) as f:
-			print("Found saved game list for user", user)
-			for line in f.readlines():
-				yield line.strip()
-	except IOError as e:
-		if e.errno != errno.ENOENT: raise
-		html = get_html('/user/%s/games/owns' % user)
-		with open(tmpfile, 'w') as f:
-			for game in get_games(html):
-				f.write("%s\n" % game)
-				yield game
-		os.rename(tmpfile, userfile)
-
-##################################################################################
-
 api = "https://api.tesera.ru"
 batch_limit = 100
 
-def get_games2(user):
+def get_games(user):
 	offset = 0
 	while True:
 		res = requests.get(
@@ -116,7 +48,7 @@ def get_games2(user):
 		if offset == int(res.headers['x-total-pages'] )- 1: break
 		offset += 1
 
-def get_user_games2(user):
+def get_user_games(user):
 	tmpfile = '%s/%s.tmp' % (tmpdir, user)
 	userfile = '%s/%s.games' % (tmpdir, user)
 	try:
@@ -126,7 +58,7 @@ def get_user_games2(user):
 				yield line.strip()
 	except FileNotFoundError:
 		with open(tmpfile, 'w') as f:
-			for game in get_games2(user):
+			for game in get_games(user):
 				f.write("%s\n" % game)
 				yield game
 		os.rename(tmpfile, userfile)
@@ -147,12 +79,12 @@ games = defaultdict(list)
 # sequential user scan; slow
 # for user in users:
 #  	print('Scraping user %s' % user)
-#  	for game in get_user_games2(user):
+#  	for game in get_user_games(user):
 #  		games[game].append(user)
 
 def scrape_job(user):
 	print("Scraping user",user)
-	return user, list(get_user_games2(user))
+	return user, list(get_user_games(user))
 
 try:
 	jobs = [gevent.spawn(scrape_job, user) for user in users]
