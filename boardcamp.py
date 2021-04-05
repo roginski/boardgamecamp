a266f8f5548dcdd6f9a2a43054f9347d02ad957e
Evgeny Roginsky <Evgeny.Roginsky@kaspersky.com>
2021-04-05 11:03:03 +0300

    1) do not use file cache for scraped data: API is fast enought to do without cache compared to HTML scraping; 2) sort games by name instead of by i-dunno-what; 3) pylinted

--- boardcamp.py
+++ boardcamp.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 from gevent import monkey; monkey.patch_all()
 
-import os
 import csv
-import json
-import errno
 import gevent
 import requests
 from collections import defaultdict
@@ -31,25 +28,9 @@ def get_games(user):
 		if not res.json():
 			break
 		for game in res.json():
-			yield '=HYPERLINK("%s"; "%s")' % (game['game']['teseraUrl'], game['game']['title'])
+			yield game['game']['title'], game['game']['teseraUrl']
 		if offset == int(res.headers['x-total-pages']) - 1: break
 		offset += 1
-
-
-def get_user_games(user):
-	tmpfile = '%s/%s.tmp' % (tmpdir, user)
-	userfile = '%s/%s.games' % (tmpdir, user)
-	try:
-		with open(userfile) as f:
-			print("Found saved game list for user", user)
-			for line in f.readlines():
-				yield line.strip()
-	except FileNotFoundError:
-		with open(tmpfile, 'w') as f:
-			for game in get_games(user):
-				f.write("%s\n" % game)
-				yield game
-		os.rename(tmpfile, userfile)
 
 
 if __name__ == '__main__':
@@ -65,15 +46,9 @@ if __name__ == '__main__':
 
 	games = defaultdict(list)
 
-	# sequential user scan; slow
-	# for user in users:
-	#  	print('Scraping user %s' % user)
-	#  	for game in get_user_games(user):
-	#  		games[game].append(user)
-
 	def scrape_job(user):
 		print("Scraping user", user)
-		return user, list(get_user_games(user))
+		return user, list(get_games(user))
 
 	jobs = [gevent.spawn(scrape_job, user) for user in users]
 	gevent.joinall(jobs)
@@ -83,28 +58,9 @@ if __name__ == '__main__':
 		for game in user_games:
 			games[game].append(user)
 
-	# try:
-	# 	with open('oldgames.csv') as f:
-	# 		oldgames = dict(((row[1], row) for row in csv.reader(f)))
-	# except FileNotFoundError:
-	# 	oldgames = {}
-
-	# for game, owners in games.items():
-	# 	if game in oldgames:
-	# 		oldowners = set(oldgames[game][0].split(','))
-	# 		oldowners.update(owners)
-	# 		oldgames[game][0] = ','.join(oldowners)
-	# 	else:
-	# 		oldgames[game] = [','.join(owners), game, '', '', '', '', '', '', '', '', '', '', '', '', '']
-
-	# with open('newgames.csv', 'w') as f:
-	# 	writer = csv.writer(f)
-	# 	for row in oldgames.values():
-	# 		writer.writerow(row)
-
 	with open(csvfile, 'w') as f:
 		writer = csv.writer(f)
-		for game, users in sorted(games.items()):
-			writer.writerow([game, ','.join(users)])
+		for game, users in sorted(games.items(), key=lambda game: game[0]):
+			writer.writerow([f'=HYPERLINK("{game[1]}"; "{game[0]}")', ','.join(users)])
 
 	print("Successfully created file", csvfile)
